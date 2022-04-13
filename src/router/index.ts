c85577a64e5d57ec4a0a325b333170d3216b7790
wyllisMonteiro <wyllismonteiro@gmail.com>
2022-04-13 15:38:29 +0200

    feat: MacroNutrientsNeedView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CaloricNeedView from '../views/CaloricNeedView.vue'
+import MacroNutrientsNeedView from '../views/MacroNutrientsNeedView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/caloric-need',
     name: 'caloric-need',
     component: CaloricNeedView
+  },
+  {
+    path: '/macronutrients-need',
+    name: 'macronutrients-need',
+    component: MacroNutrientsNeedView
   }
 ]
 
---
+++ src/views/MacroNutrientsNeedView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="macronutrients-need-view">
+    <div style="max-width: 1280px;margin: 35px auto 0 auto;" class="container">
+        <h1>Besoin en macro-nutriments</h1>
+
+        <div class="alert alert-danger" role="alert" id="error-compute">
+        </div>
+
+        <form>
+            <div class="mb-2">
+            <label for="weight" class="form-label">Poids (kg)</label>
+            <input type="number" class="form-control" id="weight" value="77">
+            </div>
+            <div class="mb-2">
+            <label for="caloric-need" class="form-label">Nombre de kcal par jour (kcal)</label>
+            <input type="number" class="form-control" id="caloric-need" value="1900">
+            </div>
+            <button class="btn btn-primary" id="compute-btn">Calculer</button>
+        </form>
+
+        <div class="alert alert-primary" role="alert" id="macronutrient-need">
+        </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {
+  }
+})
+export default class MacroNutrientsNeedView extends Vue {}
+</script>
