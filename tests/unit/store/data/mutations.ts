9276f3ba1decc1ea4668853222aed1f8b0a9c960
wyllisMonteiro <wyllismonteiro@gmail.com>
2022-05-28 03:23:20 +0200

    test: store mutations organizemeal.spec.ts

--- tests/unit/store/data/mutations.ts
+++ tests/unit/store/data/mutations.ts
@@ -1,8 +1,44 @@
 import { Food } from '@/domain/food'
-import { OrganizeMealState } from '@/domain/store/OrganizeMealState'
+import { OrganizeMealState, SelectedFood } from '@/domain/store/OrganizeMealState'
 
-export const mutations = {
+export default {
+  SET_FOOD_QUANTITY (state: OrganizeMealState, param: SelectedFood) {
+    for (let index = 0; index < Object.values(state.selectedFoods).length; index++) {
+      const food = state.selectedFoods[index]
+      if (food.food.name === param.food.name) {
+        state.selectedFoods[index].quantity = param.quantity
+        return
+      }
+    }
+
+    state.selectedFoods.push({
+      food: param.food,
+      quantity: param.quantity
+    })
+  },
+
+  SET_CURRENT_FOOD (state: OrganizeMealState, food: Food) {
+    state.currentFood = food
+  },
+
   REMOVE_CURRENT_FOOD (state: OrganizeMealState, food: Food) {
     state.selectedFoods = state.selectedFoods.filter(item => item.food.name !== food.name)
+  },
+
+  SET_FILTER (state: OrganizeMealState, filterName: string) {
+    switch (filterName) {
+      case 'energy':
+        state.activeFilters.energy = !state.activeFilters.energy
+        break
+      case 'protein':
+        state.activeFilters.protein = !state.activeFilters.protein
+        break
+      case 'lipid':
+        state.activeFilters.lipid = !state.activeFilters.lipid
+        break
+      case 'carbohydrate':
+        state.activeFilters.carbohydrate = !state.activeFilters.carbohydrate
+        break
+    }
   }
 }
--- tests/unit/store/organizemeal.spec.ts
+++ tests/unit/store/organizemeal.spec.ts
@@ -1,5 +1,5 @@
 import { state } from './data/state'
-import { mutations } from './data/mutations'
+import mutations from './data/mutations'
 import getters from './data/getters'
 
 describe('getters', () => {
@@ -174,5 +174,177 @@ describe('getters', () => {
 
     const hasNoActiveFilters = getters.hasNoActiveFilters(state)
     expect(hasNoActiveFilters).toEqual(expectedValue)
+  })
+})
+
+describe('mutations', () => {
+  it('SET_FOOD_QUANTITY : push non existing selectedFood', () => {
+    const nameFirst = 'name 1'
+    const quantity = 200
+    const selectedFood = {
+      food: {
+        name: 'name 1',
+        title: 'string',
+        weightValue: null,
+        weightUnity: 'string',
+        nutriScore: 'string',
+        companyName: 'string',
+        nutritionalValues: {
+          portionValue: 0,
+          portionUnity: 'string',
+          energy: 0,
+          energyUnity: 'string',
+          protein: 0,
+          lipid: 0,
+          carbohydrate: 0
+        }
+      },
+      quantity: quantity
+    }
+
+    state.selectedFoods.pop()
+    state.selectedFoods.pop()
+
+    mutations.SET_FOOD_QUANTITY(state, selectedFood)
+    expect(state.selectedFoods.length).toEqual(1)
+    expect(state.selectedFoods[0].food.name).toEqual(nameFirst)
+    expect(state.selectedFoods[0].quantity).toEqual(quantity)
+  })
+
+  it('SET_FOOD_QUANTITY : push existing selectedFood with quantity 500', () => {
+    const nameFirst = 'name 1'
+    const quantity = 500
+    const selectedFood = {
+      food: {
+        name: 'name 1',
+        title: 'string',
+        weightValue: null,
+        weightUnity: 'string',
+        nutriScore: 'string',
+        companyName: 'string',
+        nutritionalValues: {
+          portionValue: 0,
+          portionUnity: 'string',
+          energy: 0,
+          energyUnity: 'string',
+          protein: 0,
+          lipid: 0,
+          carbohydrate: 0
+        }
+      },
+      quantity: quantity
+    }
+
+    mutations.SET_FOOD_QUANTITY(state, selectedFood)
+    expect(state.selectedFoods.length).toEqual(1)
+    expect(state.selectedFoods[0].food.name).toEqual(nameFirst)
+    expect(state.selectedFoods[0].quantity).toEqual(quantity)
+  })
+
+  it('SET_CURRENT_FOOD : set a food', () => {
+    const currentFoodName = 'currentFood'
+    const currentFood = {
+      name: currentFoodName,
+      title: 'string',
+      weightValue: null,
+      weightUnity: 'string',
+      nutriScore: 'string',
+      companyName: 'string',
+      nutritionalValues: {
+        portionValue: 0,
+        portionUnity: 'string',
+        energy: 0,
+        energyUnity: 'string',
+        protein: 0,
+        lipid: 0,
+        carbohydrate: 0
+      }
+    }
+
+    mutations.SET_CURRENT_FOOD(state, currentFood)
+    expect(state.currentFood).toBe(currentFood)
+  })
+
+  it('REMOVE_CURRENT_FOOD : not food found', () => {
+    const foodNameFirst = 'name 1'
+    const currentFoodName = 'not found'
+    const currentFood = {
+      name: currentFoodName,
+      title: 'string',
+      weightValue: null,
+      weightUnity: 'string',
+      nutriScore: 'string',
+      companyName: 'string',
+      nutritionalValues: {
+        portionValue: 0,
+        portionUnity: 'string',
+        energy: 0,
+        energyUnity: 'string',
+        protein: 0,
+        lipid: 0,
+        carbohydrate: 0
+      }
+    }
+
+    expect(state.selectedFoods.length).toEqual(1)
+    expect(state.selectedFoods[0].food.name).toEqual(foodNameFirst)
+
+    mutations.REMOVE_CURRENT_FOOD(state, currentFood)
+    expect(state.selectedFoods.length).toEqual(1)
+    expect(state.selectedFoods[0].food.name).toEqual(foodNameFirst)
+  })
+
+  it('REMOVE_CURRENT_FOOD : success', () => {
+    const foodNameFirst = 'name 1'
+    const currentFood = {
+      name: foodNameFirst,
+      title: 'string',
+      weightValue: null,
+      weightUnity: 'string',
+      nutriScore: 'string',
+      companyName: 'string',
+      nutritionalValues: {
+        portionValue: 0,
+        portionUnity: 'string',
+        energy: 0,
+        energyUnity: 'string',
+        protein: 0,
+        lipid: 0,
+        carbohydrate: 0
+      }
+    }
+
+    expect(state.selectedFoods.length).toEqual(1)
+    expect(state.selectedFoods[0].food.name).toEqual(foodNameFirst)
+
+    mutations.REMOVE_CURRENT_FOOD(state, currentFood)
+    expect(state.selectedFoods.length).toEqual(0)
+  })
+
+  it('SET_FILTER : energy filter to false', () => {
+    const filterName = 'energy'
+
+    expect(state.activeFilters.energy).toEqual(true)
+
+    mutations.SET_FILTER(state, filterName)
+    expect(state.activeFilters.energy).toEqual(false)
+  })
+
+  it('SET_FILTER : energy filter to true', () => {
+    const filterName = 'energy'
+
+    expect(state.activeFilters.energy).toEqual(false)
+
+    mutations.SET_FILTER(state, filterName)
+    expect(state.activeFilters.energy).toEqual(true)
+  })
+
+  it('SET_FILTER : lipid filter to false', () => {
+    const filterName = 'lipid'
+
+    expect(state.activeFilters.lipid).toEqual(true)
+
+    mutations.SET_FILTER(state, filterName)
+    expect(state.activeFilters.lipid).toEqual(false)
   })
 })
